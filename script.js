88e6182f2296247b66840af067204ced1f5e2232
vikasyadav01hp <vikasyadav01.hp@outlook.com>
2024-08-05 18:12:44 +0530

    four commit

--- script.js
+++ script.js
@@ -1,116 +1,80 @@
-document.getElementById("add").addEventListener("click", function () {
-    console.log("Adding new box");
+$(document).ready(function() {
+    let itemCounter = 1;
 
-    const content = document.getElementById("content");
+    $('#add-browser').click(function() {
+        console.log('Add Browser button clicked.');
 
-    const newBox = document.createElement("div");
-    newBox.className = "box";
-    newBox.innerHTML = `
-      <input type="file" accept="image/*" class="file-input">
-      <select class="img-type">
-        <option value="primary">Primary Image</option>
-        <option value="secondary">Secondary Image</option>
-      </select>
-      <button class="remove btn btn-dark">Remove</button>
-      <img src="" class="thumb" style="display: none;" />
-    `;
-
-    content.appendChild(newBox);
-    console.log("New box added");
-
-    newBox.querySelector(".file-input").addEventListener("change", function (event) {
-        console.log("File input changed");
-        showImage(event);
-    });
-
-    newBox.querySelector(".img-type").addEventListener("change", function () {
-        console.log("Image type changed to:", this.value);
-        typeChange(this);
-    });
-
-    setPrimary();
-    console.log("Primary image set");
-});
-
-function showImage(event) {
-    console.log("Showing image");
-
-    const input = event.target;
-    const file = input.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        console.log("Image loaded");
-        const thumb = input.nextElementSibling.nextElementSibling.nextElementSibling;
-        thumb.src = e.target.result;
-        thumb.style.display = "block";
-    };
-
-    if (file) {
-        reader.readAsDataURL(file);
-    }
-}
-
-function typeChange(select) {
-    console.log("Handling image type change");
-    const newType = select.value;
-    
-    if (newType === "primary") {
-        console.log("New type is primary");
-        const prevPrimary = document.querySelector(".box .img-type[value='primary']");
-        if (prevPrimary && prevPrimary !== select) {
-            console.log("Changing previous primary to secondary");
-            prevPrimary.value = "secondary";
-        }
-    }
-
-    setPrimary();
-    console.log("Primary image set after type change");
-}
-
-function setPrimary() {
-    console.log("Setting primary image");
-
-    const types = document.querySelectorAll(".img-type");
-    let hasPrimary = false;
-
-    types.forEach(select => {
-        if (select.value === "primary") {
-            if (hasPrimary) {
-                console.log("Changing duplicate primary to secondary");
-                select.value = "secondary";
-            } else {
-                hasPrimary = true;
+        let allImagesSelected = true;
+        $('.file-input').each(function() {
+            if (!$(this).val()) {
+                allImagesSelected = false;
             }
+        });
+
+        if (!allImagesSelected) {
+
+            alert('First Add image for Preview then add another');
+            return; 
+        }
+
+        itemCounter++;
+        console.log(`Creating new item with counter: ${itemCounter}`);
+
+        let newItemHtml = `
+            <div class="browser-item" id="browser-item-${itemCounter}">
+                <input type="file" class="file-input" />
+                <img src="img.jpg" alt="Browser Image" class="browser-img">
+                <select id="browser-select-${itemCounter}" class="browser-select">
+                    <option value="primary">Primary</option>
+                    <option value="secondary" selected>Secondary</option>
+                </select>
+                <span class="delete-btn">Remove</span>
+            </div>
+        `;
+        
+        $('#browser-list').append(newItemHtml);
+        console.log('New item added to the browser list.');
+    });
+
+    $('#browser-list').on('click', '.delete-btn', function() {
+        console.log('Delete button clicked.');
+
+        let parentItem = $(this).parent();
+        let isPrimary = parentItem.find('.browser-select').val() === 'primary';
+
+        if (isPrimary) {
+            alert('You cannot delete the primary browser.');
+            console.log('Attempted to delete a primary browser.');
+        } else {
+            parentItem.remove();
+            console.log('Item removed from the browser list.');
         }
     });
 
-    if (!hasPrimary) {
-        console.log("No primary image found, setting first image as primary");
-        const first = document.querySelector(".box .img-type");
-        if (first) {
-            first.value = "primary";
-        }
-    }
-}
+    $('#browser-list').on('change', '.browser-select', function() {
+        console.log('Select dropdown changed.');
 
-document.getElementById("content").addEventListener("change", function (event) {
-    if (event.target.classList.contains("file-input")) {
-        console.log("File input changed (delegated)");
-        showImage(event);
-    }
-});
-
-document.getElementById("content").addEventListener("click", function (event) {
-    if (event.target.classList.contains("remove")) {
-        console.log("Remove button clicked");
-        const box = event.target.parentElement;
-        const typeSelect = box.querySelector(".img-type");
-        if (typeSelect.value === "primary") {
-            alert("You cannot remove the primary image.");
-        } else {
-            console.log("Removing box");
-            box.remove();
+        if ($(this).val() === 'primary') {
+            console.log('Primary selected. Changing all other selects to secondary.');
+            $('.browser-select').not(this).val('secondary');
         }
-    }
+    });
+
+    $('#browser-list').on('change', '.file-input', function(event) {
+        console.log('File input changed.');
+
+        let reader = new FileReader();
+        let file = event.target.files[0];
+        let imgElement = $(this).siblings('.browser-img');
+
+        reader.onload = function(e) {
+            imgElement.attr('src', e.target.result);
+            console.log('Image updated.');
+        }
+
+        if (file) {
+            reader.readAsDataURL(file);
+            console.log('Reading file as Data URL.');
+        }
+    });
 });
